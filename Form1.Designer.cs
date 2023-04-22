be1ea6249c09314a5806d012852a7b47811e3b12
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2023-04-21 20:21:37 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace Stadium_Seating
+{
+    partial class StadiumSeating
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tboxClassA = new System.Windows.Forms.TextBox();
+            this.tboxClassB = new System.Windows.Forms.TextBox();
+            this.tboxClassC = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblOutputClassC = new System.Windows.Forms.Label();
+            this.lblOutputClassB = new System.Windows.Forms.Label();
+            this.lblOutputClassA = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnCalculateRevenue = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.tboxClassA);
+            this.groupBox1.Controls.Add(this.tboxClassB);
+            this.groupBox1.Controls.Add(this.tboxClassC);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(56, 37);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(311, 234);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tickets Sold";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("NanumGothicCoding", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(11, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(294, 51);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Enter the number of tickets sold for each class of seats";
+            // 
+            // tboxClassA
+            // 
+            this.tboxClassA.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tboxClassA.Font = new System.Drawing.Font("NanumGothicCoding", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tboxClassA.Location = new System.Drawing.Point(122, 97);
+            this.tboxClassA.Name = "tboxClassA";
+            this.tboxClassA.Size = new System.Drawing.Size(157, 28);
+            this.tboxClassA.TabIndex = 1;
+            // 
+            // tboxClassB
+            // 
+            this.tboxClassB.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tboxClassB.Font = new System.Drawing.Font("NanumGothicCoding", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tboxClassB.Location = new System.Drawing.Point(122, 139);
+            this.tboxClassB.Name = "tboxClassB";
+            this.tboxClassB.Size = new System.Drawing.Size(157, 28);
+            this.tboxClassB.TabIndex = 2;
+            // 
+            // tboxClassC
+            // 
+            this.tboxClassC.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tboxClassC.Font = new System.Drawing.Font("NanumGothicCoding", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tboxClassC.Location = new System.Drawing.Point(122, 184);
+            this.tboxClassC.Name = "tboxClassC";
+            this.tboxClassC.Size = new System.Drawing.Size(157, 28);
+            this.tboxClassC.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(29, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 19);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Class C:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(29, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 19);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Class B:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Class A:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblTotal);
+            this.groupBox2.Controls.Add(this.lblOutputClassC);
+            this.groupBox2.Controls.Add(this.lblOutputClassB);
+            this.groupBox2.Controls.Add(this.lblOutputClassA);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(391, 37);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(310, 234);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Revenue Generated";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTotal.Font = new System.Drawing.Font("NanumGothicCoding", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(135, 178);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(136, 32);
+            this.lblTotal.TabIndex = 0;
+            this.lblTotal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblOutputClassC
+            // 
+            this.lblOutputClassC.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblOutputClassC.Font = new System.Drawing.Font("NanumGothicCoding", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutputClassC.Location = new System.Drawing.Point(135, 135);
+            this.lblOutputClassC.Name = "lblOutputClassC";
+            this.lblOutputClassC.Size = new System.Drawing.Size(136, 32);
+            this.lblOutputClassC.TabIndex = 0;
+            this.lblOutputClassC.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblOutputClassB
+            // 
+            this.lblOutputClassB.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblOutputClassB.Font = new System.Drawing.Font("NanumGothicCoding", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutputClassB.Location = new System.Drawing.Point(135, 93);
+            this.lblOutputClassB.Name = "lblOutputClassB";
+            this.lblOutputClassB.Size = new System.Drawing.Size(136, 32);
+            this.lblOutputClassB.TabIndex = 0;
+            this.lblOutputClassB.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblOutputClassA
+            // 
+            this.lblOutputClassA.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblOutputClassA.Font = new System.Drawing.Font("NanumGothicCoding", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutputClassA.Location = new System.Drawing.Point(135, 51);
+            this.lblOutputClassA.Name = "lblOutputClassA";
+            this.lblOutputClassA.Size = new System.Drawing.Size(136, 32);
+            this.lblOutputClassA.TabIndex = 0;
+            this.lblOutputClassA.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(53, 187);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(63, 19);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Total:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(35, 142);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(81, 19);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Class C:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(35, 97);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 19);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Class B:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(35, 51);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 19);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Class A:";
+            // 
+            // btnCalculateRevenue
+            // 
+            this.btnCalculateRevenue.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculateRevenue.Location = new System.Drawing.Point(189, 310);
+            this.btnCalculateRevenue.Name = "btnCalculateRevenue";
+            this.btnCalculateRevenue.Size = new System.Drawing.Size(110, 59);
+            this.btnCalculateRevenue.TabIndex = 4;
+            this.btnCalculateRevenue.Text = "Calculate Revenue";
+            this.btnCalculateRevenue.UseVisualStyleBackColor = true;
+            this.btnCalculateRevenue.Click += new System.EventHandler(this.btnCalculateRevenue_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(327, 310);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(110, 59);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(465, 310);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(110, 59);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // StadiumSeating
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 417);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculateRevenue);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "StadiumSeating";
+            this.Text = "Stadium Seating";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tboxClassA;
+        private System.Windows.Forms.TextBox tboxClassB;
+        private System.Windows.Forms.TextBox tboxClassC;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblOutputClassC;
+        private System.Windows.Forms.Label lblOutputClassB;
+        private System.Windows.Forms.Label lblOutputClassA;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnCalculateRevenue;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stadium_Seating
+{
+    public partial class StadiumSeating : Form
+    {
+        const double CLASS_A = 15;
+        const double CLASS_B = 12;
+        const double CLASS_C = 9;
+
+        private double totalA = 0;
+        private double totalB = 0;
+        private double totalC = 0;
+        private double total = 0;
+
+        public StadiumSeating()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculateRevenue_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                totalA = double.Parse(tboxClassA.Text) * CLASS_A;
+                lblOutputClassA.Text = totalA.ToString("c2");
+
+                totalB = double.Parse(tboxClassB.Text) * CLASS_B;
+                lblOutputClassB.Text = totalB.ToString("c2");
+
+                totalC = double.Parse(tboxClassC.Text) * CLASS_C;
+                lblOutputClassC.Text = totalC.ToString("c2");
+
+                total = totalA + totalB + totalC;
+                lblTotal.Text = total.ToString("c2");
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            
+            lblOutputClassA.Text = "";
+            
+            lblOutputClassB.Text = "";
+            
+            lblOutputClassC.Text = "";
+            
+            lblTotal.Text = "";
+
+            tboxClassA.Text = "";
+            tboxClassB.Text = "";
+            tboxClassC.Text = "";
+
+            tboxClassA.Focus();
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
